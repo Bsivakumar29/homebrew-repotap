25925452e277c4e8ab02995960f664f55c8e82b1
Kevin <kevin@Kevins-MacBook-Pro.local>
2022-02-21 18:43:06 +0530

    second formula

--- repotap.rb
+++ repotap.rb
@@ -4,8 +4,8 @@
 class Repotap < Formula
   desc "test_formula"
   homepage "https://github.com/Bsivakumar29/repotap"
-  url "https://github.com/Bsivakumar29/repotap/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "1b6fe31490238d4c1d7835a5e2b4c1eb7062ca9b4670bdc1cfa5e1c2d0bd3767"
+  url "https://github.com/Bsivakumar29/repotap/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "1827e33767a379162921de31bfba01bf6d33743cf36d45d7307851e7a65f110d"
   license ""
 
   def install
