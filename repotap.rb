cb00f3c195c66ca092c78ba6263fe16c14712a7a
Kevin <kevin@Kevins-MacBook-Pro.local>
2022-02-21 20:36:04 +0530

    prefix added

--- repotap.rb
+++ repotap.rb
@@ -3,9 +3,9 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Repotap < Formula
   desc "test_formula"
-  homepage ""
-  url "https://github.com/Bsivakumar29/repotap/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "06f0254a66db23d6862c5516a37c6d948bd2c02ecd7c4a4d58bbb7187fab3134"
+  homepage "https://github.com/Bsivakumar29/repotap"
+  url "https://github.com/Bsivakumar29/repotap/archive/refs/tags/v1.1.5.tar.gz"
+  sha256 "8386c4d331de944b80f219deada5c25b5f2ab2f90c282d433f8bd7f22392d0fd"
   license ""
 
   def install
